664e20396937c10054031d75585450f87bb72df9
Adam Beckingham <beckadamtheartist@gmail.com>
2022-10-27 21:10:52 -0700

    Re-add files I missed part 2

---
+++ java/ModCraftingTableBlock.java
@@ -0,0 +1,10 @@
+package ${mod.package}.blocks;
+
+import net.minecraft.block.CraftingTableBlock;
+
+public class ModCraftingTableBlock extends CraftingTableBlock {
+
+    public ModCraftingTableBlock(Settings settings) {
+        super(settings);
+    }
+}
